224f1cdffd62efa3d3536d5e5b1afb56657ed239
mIzadyAmiri <mojtabaizady99@gmail.com>
2024-09-18 11:19:42 +0330

    main component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,47 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+
+const iniState = {
+  questions: [],
+  status: 'loading', // loading, error, ready, active, finished
+};
+
+function quizReducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('invalid action type');
+  }
+}
+
 export default function App() {
-  return <div></div>;
+  const [{ questions, status }, dispatch] = useReducer(quizReducer, iniState);
+
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    fetch('http://localhost:8000/questions')
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed', payload: err }));
+  }, []);
+
+  return (
+    <div className='app'>
+      <Header />
+
+      <Main className='main'>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+      </Main>
+    </div>
+  );
 }
